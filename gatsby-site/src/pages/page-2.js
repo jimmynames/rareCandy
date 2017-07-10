815a5a4f385c07f936525cb37b24f87af89e5c17
Names <names@jimmy.home>
2017-07-10 23:03:56 +0100

    working styled components via searching this error --> ypeError: _gatsbyPluginStyledComponents2.default.div is not a function and usinf this link https://github.com/styled-components/styled-components/issues/301

--- gatsby-site/src/pages/page-2.js
+++ gatsby-site/src/pages/page-2.js
@@ -1,20 +1,20 @@
 import React from "react"
 import Link from "gatsby-link"
-import StyledComponents from "gatsby-plugin-styled-components"
+const styled = require('styled-components').default
 import Helmet from "react-helmet"
-//
-// const Lol = styled.div`
-//   background-color: pink;
-// `
+
+const Lol = styled.div`
+  background-color: pink;
+`
 
 export default class Index extends React.Component {
   render() {
     return (
-      <div>
+      <Lol>
         <h1>Hi people</h1>
         <p>Welcome to page 2</p>
         <Link to="/">Go back to the homepage</Link>
-      </div>
+      </Lol>
     )
   }
 }
